8c41f128620c20d0c97c771f75ebe960f0a6367a
Darren Leonard <e@eeeeee.uk>
2021-07-10 17:45:57 +0100

    Passed first test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,15 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {Appointment} from "../src/Appointment"
+let container
+// let customer
+
+const render = component => ReactDOM.render(component,container)
+const getCustomerData = firstName => ({firstName: firstName})
+
+beforeEach(()=>{
+    container = document.createElement('div');
+})
 
 describe('Appointment', ()=>{
     it('renders the customer first name', () => {
-        const customer = {firstName: "Ashley"}
-        const component= <Appointment customer={customer} />
-        const container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(component,container)
+        const customerName = "Ashley"
+        render(<Appointment customer={getCustomerData(customerName)} />)
+        expect(container.textContent).toMatch(customerName)
+    })
 
-        expect(document.body.textContent).toMatch("Ashley")
+    it('renders another customer first name', () => {
+        const customerName = "Jordan"
+        render(<Appointment customer={getCustomerData(customerName)} />)
+        expect(container.textContent).toMatch(customerName)
     })
 })
