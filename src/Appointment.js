9014357b3c1d039fcd985149fb7d171f398bf669
Darren Leonard <e@eeeeee.uk>
2021-07-11 12:15:50 +0100

    c1-48

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,2 +1,16 @@
 import React from "react"
 export const Appointment = ({customer: {firstName}})=><div>{firstName}</div>
+export const AppointmentsDayView = ({appointments})=>(
+    <div id="appointmentsDayView">
+        <ol>
+            {appointments.map(app=>(
+                <li key={app.startsAt}>{appointmentTimeOfDay(app.startsAt)}</li>
+            ))}
+        </ol>
+    </div>
+)
+
+const appointmentTimeOfDay = startsAt => {
+    const [h,m] = new Date(startsAt).toTimeString().split(':')
+    return `${h}:${m}`
+}
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,17 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {Appointment} from "../src/Appointment"
+import {
+    Appointment,
+    AppointmentsDayView
+} from "../src/Appointment"
 let container
-// let customer
+
 
 const render = component => ReactDOM.render(component,container)
-const getCustomerData = firstName => ({firstName: firstName})
 
 beforeEach(()=>{
     container = document.createElement('div');
 })
-
 describe('Appointment', ()=>{
+    const getCustomerData = firstName => ({firstName: firstName})
+
     it('renders the customer first name', () => {
         const customerName = "Ashley"
         render(<Appointment customer={getCustomerData(customerName)} />)
@@ -22,5 +25,37 @@ describe('Appointment', ()=>{
         const customerName = "Jordan"
         render(<Appointment customer={getCustomerData(customerName)} />)
         expect(container.textContent).toMatch(customerName)
+    })
+})
+
+describe("AppointmentsDayView", ()=> {
+
+    const today = new Date()
+
+    const appointments = [
+        {startsAt: today.setHours(12, 0)},
+        {startsAt: today.setHours(13,0)},
+        {startsAt: today.setHours(14,0)}
+    ]
+
+    const appointmentTimeOfDay = startsAt => {
+        const [h,m] = new Date(startsAt).toTimeString().split(':')
+        return `${h}:${m}`
+    }
+
+    it("renders a div with right id", ()=>{
+        render(<AppointmentsDayView appointments={[]} />)
+        expect(container.querySelector('div#appointmentsDayView')).not.toBeNull()
+    })
+
+    it("renders multiple appointments in an ol element",() =>{
+        render(<AppointmentsDayView appointments={appointments} />)
+        expect(container.querySelector('ol').children).toHaveLength(appointments.length)
+        appointments.forEach((appointment,index)=>{
+            expect(container.querySelectorAll('li')[index].textContent).toEqual(appointmentTimeOfDay(appointment.startsAt))
+
+        })
+        // expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00')
+        // expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00')
     })
 })
